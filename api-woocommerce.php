46822e5d8c819ca5447c820c3751e0e6ad505e1a
Jose Carreres <jose.carreres.81@gmail.com>
2022-06-08 21:55:54 +0200

    Create functions Activate(), Deactivate(), Uninstall

--- api-woocommerce.php
+++ api-woocommerce.php
@@ -1,1 +1,24 @@
 <?php
+
+/*
+* Plugin name: API Woocommerce
+* Description: Woocommerce api test plugin
+* Version: 0.0.1
+*/
+
+function Activate() {
+
+}
+
+function Deactivate() {
+
+}
+
+function Delete() {
+
+}
+
+echo 'Holaaaa';
+
+register_activation_hook(__FILE__, 'Activate');
+register_deactivation_hook(__FILE__, 'Deactivate');
