b8217a1ee224bc3f4b70a269c1f263e0fd78587c
Jose Carreres <jose.carreres.81@gmail.com>
2022-06-08 23:44:34 +0200

    creation of the administration menu

--- api-woocommerce.php
+++ api-woocommerce.php
@@ -14,11 +14,28 @@ function Deactivate() {
 
 }
 
-function Delete() {
 
-}
-
-echo 'Holaaaa';
 
 register_activation_hook(__FILE__, 'Activate');
 register_deactivation_hook(__FILE__, 'Deactivate');
+
+
+
+// Administration menu
+add_action('admin_menu', 'createMenu');
+
+function createMenu() {
+    add_menu_page(
+        'API Woocommerce', //title page
+        'API Woocommerce', //title menu
+        'manage_options', //capability
+        plugin_dir_path(__FILE__).'admin/products.php', //slug
+        null, //content function
+        plugin_dir_url(__FILE__).'admin/img/api.png', //image url
+        '1' //menu position
+    );
+}
+
+function ShowContent() {
+    echo '<h1>API Woocommerce Products</h1>';
+}
